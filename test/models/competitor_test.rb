5937149c0068984fd523bd536bc3daa4abd02198
Matt Mehlhope <mmehlhope@gmail.com>
2013-12-19 08:24:41 -0800

    Adding elo calculation tests to competitor tests

--- test/models/competitor_test.rb
+++ test/models/competitor_test.rb
@@ -2,27 +2,48 @@ require 'test_helper'
 
 class CompetitorTest < ActiveSupport::TestCase
 
-  test "increment win count" do
+  test "should increment win count" do
     competitor = Competitor.new
-    assert_equal(0, competitor.wins, "Competitor has no wins")
+    assert_equal 0, competitor.wins, "Competitor has no wins"
 
     competitor.increment_win_count
-    assert_equal(1, competitor.wins, "Competitor has 1 win")
+    assert_equal 1, competitor.wins, "Competitor has 1 win"
   end
 
-  test "reduce win count" do
+  test "should reduce win count" do
     competitor = Competitor.new(:wins => 1)
-    assert_equal(1, competitor.wins, "Competitor has 1 win")
+    assert_equal 1, competitor.wins, "Competitor has 1 win"
 
     competitor.reduce_win_count
-    assert_equal(0, competitor.wins, "Competitor has no wins")
+    assert_equal 0, competitor.wins, "Competitor has no wins"
   end
 
-  test "update rating" do
+  test "should calculate winning player elo" do
     competitor = Competitor.new(:rating => 1000)
-    assert_equal(1000, competitor.rating,  "Competitor has a rating of 1000")
+    opponent   = Competitor.new(:rating => 1100)
+    assert_equal 1000, competitor.rating, "Competitor default rating"
+    assert_equal 1100, opponent.rating, "Opponent default rating"
+    # calculate new Elo rating for competitor victory
+    new_rating = competitor.calculate_elo(opponent.rating, 1)
+    assert_equal 1020, new_rating, "Competitor ELO increased to 1020"
+  end
+
+  test "should calculate losing player elo" do
+    competitor = Competitor.new(:rating => 1000)
+    opponent   = Competitor.new(:rating => 1100)
+    assert_equal 1000, competitor.rating, "Competitor default rating"
+    assert_equal 1100, opponent.rating, "Opponent default rating"
+    # calculate new Elo rating for competitor victory
+    new_rating = competitor.calculate_elo(opponent.rating, 0)
+    assert_equal 988, new_rating, "Competitor ELO decreased to 988"
+  end
+
+
+  test "should update competitor rating" do
+    competitor = Competitor.new(:rating => 1000)
+    assert_equal 1000, competitor.rating,  "Competitor has a rating of 1000"
 
     competitor.update_rating(1200)
-    assert_equal(1200, competitor.rating, "Competitor's rating updated")
+    assert_equal 1200, competitor.rating, "Competitor's rating updated"
   end
 end
