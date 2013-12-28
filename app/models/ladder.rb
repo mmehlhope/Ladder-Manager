a23d1b0d57050e73b8f4578b81d40e62ee814bb5
Matt Mehlhope <mmehlhope@gmail.com>
2013-12-27 20:40:52 -0800

    Ladder name validation and tests

--- app/models/ladder.rb
+++ app/models/ladder.rb
@@ -2,5 +2,7 @@ class Ladder < ActiveRecord::Base
   has_many :competitors, dependent: :destroy
   has_many :matches, dependent: :destroy
 
-  validates :name, presence: true
+  validates :name, presence: true, format: {
+    with: /\A[a-zA-Z0-9 ]+\z/, message: "can only contain letters, numbers, and spaces"
+  }
 end
--- test/models/ladder_test.rb
+++ test/models/ladder_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class LadderTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "ladder has valid name" do
+    ladder = Ladder.new(name: "Great ladder")
+    assert ladder.valid?, "Ladder name is valid"
+  end
+
+  test "Ladder has invalid name" do
+    ladder = Ladder.new(name: "Great ladder; mysql()")
+    assert !ladder.valid?
+  end
 end
