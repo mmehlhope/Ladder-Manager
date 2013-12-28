43012a110a1e4e8f59273a2d97be0e38ca815fef
Matt Mehlhope <mmehlhope@gmail.com>
2013-12-27 20:47:22 -0800

    Updating ladder name regex

--- app/models/ladder.rb
+++ app/models/ladder.rb
@@ -3,6 +3,6 @@ class Ladder < ActiveRecord::Base
   has_many :matches, dependent: :destroy
 
   validates :name, presence: true, format: {
-    with: /\A[a-zA-Z0-9 ]+\z/, message: "can only contain letters, numbers, and spaces"
+    with: /\A[a-zA-Z0-9]*[a-zA-Z][a-zA-Z0-9 ]*\z/, message: "can only contain letters, numbers, and spaces."
   }
 end
