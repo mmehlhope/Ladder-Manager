98290d6c9632c5adcb8914c5c258aa31a8ecec13
Matt Mehlhope <mmehlhope@gmail.com>
2013-12-31 00:19:36 -0800

    Updating status messages on session modification

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,20 +7,23 @@ class SessionsController < ApplicationController
   def create
     if @ladder && @ladder.authenticate(params[:password])
       session[:user_can_admin] = [@ladder.id]
+      flash[:success] = "You have been successfully logged in."
     else
-      flash[:error] = "Invalid password"
+      flash[:error] = "The password you entered was invalid."
     end
     redirect_to ladder_path(@ladder)
   end
 
   def destroy
     session[:user_can_admin] = nil
+    flash[:success] = "You have been successfully logged out."
     redirect_to ladder_path(@ladder)
   end
 
   private
 
     def set_ladder
-      @ladder = Ladder.find_by_id(params[:ladder_id])
+      id = params[:id] ||= params[:ladder_id]
+      @ladder = Ladder.find_by_id(id)
     end
 end
