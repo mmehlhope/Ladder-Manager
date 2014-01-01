cccd6f9c6a7e2d9dd926d2291552c1db6380af43
Matt Mehlhope <mmehlhope@gmail.com>
2013-12-31 16:48:09 -0800

    Ladder names can now contain apostrophes

--- app/models/ladder.rb
+++ app/models/ladder.rb
@@ -4,7 +4,7 @@ class Ladder < ActiveRecord::Base
   has_secure_password
 
   validates :name, presence: true, format: {
-    with: /\A[a-zA-Z0-9]*[a-zA-Z][a-zA-Z0-9 ]*\z/, message: "can only contain letters, numbers, and spaces."
+    with: /\A[a-zA-Z0-9]*[a-zA-Z][a-zA-Z0-9  ']*\z/, message: "can only contain letters, numbers, and spaces."
   }
   validates :admin_email, presence: true, format: {
     with: /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/, message: "must be a valid email address."
