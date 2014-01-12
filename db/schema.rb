eb93fe8646138c1f841fbc916c25265ef19c9688
Matt Mehlhope <mmehlhope@gmail.com>
2014-01-12 15:22:56 -0800

    Schema updates reflecting user changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140112180916) do
+ActiveRecord::Schema.define(version: 20140112230512) do
 
   create_table "competitors", force: true do |t|
     t.string   "name"
@@ -47,8 +47,6 @@ ActiveRecord::Schema.define(version: 20140112180916) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "admin_email"
-    t.text     "password_digest"
     t.integer  "user_id"
   end
 
@@ -85,5 +83,7 @@ ActiveRecord::Schema.define(version: 20140112180916) do
     t.text     "password_digest"
     t.string   "email"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", using: :btree
 
 end
