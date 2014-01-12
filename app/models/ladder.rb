4431d6852d54c1a130935fd7432a513eb2150dde
Matt Mehlhope <mmehlhope@gmail.com>
2014-01-12 15:17:15 -0800

    Updating validations for ladder and user

--- app/models/ladder.rb
+++ app/models/ladder.rb
@@ -2,12 +2,9 @@ class Ladder < ActiveRecord::Base
   belongs_to :user
   has_many :competitors, dependent: :destroy
   has_many :matches, dependent: :destroy
-  has_secure_password
 
   validates :name, presence: true, format: {
     with: /\A[a-zA-Z0-9]*[a-zA-Z][a-zA-Z0-9  ']*\z/, message: "can only contain letters, numbers, and spaces."
   }
-  validates :admin_email, presence: true, format: {
-    with: /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/, message: "must be a valid email address."
-  }
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,20 @@
 class User < ActiveRecord::Base
   has_many :ladders
+  before_validation :downcase_email
   has_secure_password
 
   validates :name, presence: true, format: {
     with: /\A[a-zA-Z0-9]*[a-zA-Z][a-zA-Z0-9']*\z/, message: "can only contain letters and numbers."
   }
-  validates :email, presence: true, format: {
+  validates :email, presence: true, uniqueness: true, format: {
     with: /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/, message: "must be a valid email address."
   }
+
+
+
+  private
+
+    def downcase_email
+      self.email = self.email.downcase if self.email.present?
+    end
 end
