6086d0d0f6efb009d3dbb758d7bea881b902c2dc
Matt Mehlhope <mmehlhope@gmail.com>
2014-01-12 15:17:37 -0800

    Updating helper methods for application given users

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,21 +1,40 @@
 module ApplicationHelper
 
-  def flash_message?
-    true if flash[:notice] || flash[:alert] || flash[:error] || flash[:success]
+  ################
+  # User helpers #
+  ################
+  def current_user
+    @current_user ||= User.find_by_id(session[:user_id])
   end
 
   def ensure_user_can_admin_ladder
     unless user_can_admin_ladder
-      flash[:error] = "Please sign in as the administrator to modify the ladder."
-      redirect_to ladder_path(@ladder)
-    end
-  end
-  def user_can_admin_ladder
-    unless @ladder.nil?
-      session[:user_can_admin].try(:include?, @ladder.id)
-    else
-      false
+      flash[:error] = "Please login to edit the ladder."
+      redirect_to login_path
     end
   end
 
+  def user_can_admin_ladder
+    # If user isn't logged in, clearly can't admin. If the user is logged in but the ladder
+    # does not belong to them, they also cannot admin it.
+    current_user.nil? ? false : (current_user.ladders.find_by_id(params[:id]).nil? ? false : true)
+  end
+
+  ####################
+  # Redirect Helpers #
+  ####################
+
+  def redirect_with_error(error, path)
+    error ||= "You do not have permission to view that page"
+    path  ||= root_path
+    flash[:error] = error
+    redirect_to path
+  end
+
+  ##################
+  # Global Helpers #
+  ##################
+  def flash_message?
+    true if flash[:notice] || flash[:alert] || flash[:error] || flash[:success]
+  end
 end
