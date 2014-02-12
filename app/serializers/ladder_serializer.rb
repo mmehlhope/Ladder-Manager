8e3f4b7f8bf1bded15f0bed6e0e205459e6b633b
Matt Mehlhope <mmehlhope@gmail.com>
2014-02-11 23:32:09 -0800

    Ladder and match serializer refactor to include properties as needed

--- app/serializers/ladder_serializer.rb
+++ app/serializers/ladder_serializer.rb
@@ -1,3 +1,26 @@
-class LadderSerializer < ActiveModel::Serializer
-  attributes :id
+class LadderSerializer < ApplicationSerializer
+  self.root = false
+
+  def attributes
+    hash = super
+    hash[:id]                = object.id
+    hash[:date_created]      = date_created
+    hash[:last_updated]      = updated_how_long_ago
+    hash[:name]              = object.name
+    hash[:organization_id]   = object.organization_id
+
+    unless options[:minimal]
+      hash[:competitors] = ActiveModel::ArraySerializer.new(
+                            object.competitors,
+                            each_serializer:
+                            CompetitorSerializer)
+      hash[:matches]     = ActiveModel::ArraySerializer.new(
+                            object.matches,
+                            each_serializer:
+                            MatchSerializer,
+                            minimal: true)
+    end
+
+    hash
+  end
 end
--- app/serializers/match_serializer.rb
+++ app/serializers/match_serializer.rb
@@ -1,25 +1,22 @@
-class MatchSerializer < ActiveModel::Serializer
+class MatchSerializer < ApplicationSerializer
   has_many :games
 
   def attributes
     hash = super
     hash[:id]                = object.id
-    hash[:date_created]      = object.date_created
-    hash[:last_updated]      = object.updated_how_long_ago
+    hash[:date_created]      = date_created
+    hash[:last_updated]      = updated_how_long_ago
     hash[:winner_id]         = object.winner_id
     hash[:finalized]         = object.finalized
-    hash[:competitor_1]      = {
-      id: object.competitor_1,
-      name: object.competitor_1_name,
-    }
-    hash[:competitor_2]      = {
-      id: object.competitor_2,
-      name: object.competitor_2_name,
-    }
-    hash[:ladder]            = {
-      ladder_id: object.ladder_id,
-      ladder_name: object.ladder.name
-    }
+    hash[:competitor_1] = CompetitorSerializer.new(Competitor.find_by_id(object.competitor_1))
+    hash[:competitor_2] = CompetitorSerializer.new(Competitor.find_by_id(object.competitor_2))
+
+    unless options[:minimal]
+      hash[:ladder] = {
+        ladder_id: object.ladder_id,
+        ladder_name: object.ladder.name
+      }
+    end
     hash
   end
 end
