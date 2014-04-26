1f2ba8833eb85f5a9996ce6d32f0ba0685398bfb
Matt Mehlhope <mmehlhope@gmail.com>
2014-04-26 13:07:13 -0700

    Validate uniqueness of email before creating account

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
   validates :name, presence: true, format: {
     with: /\A[a-zA-Z0-9]*[a-zA-Z][a-zA-Z0-9' ]*\z/, message: "can only contain letters, numbers, and spaces."
   }
-  validates :email, presence: true, format: {
+  validates :email, presence: true, uniqueness: true, format: {
     with: /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/, message: "must be a valid email address."
   }
 
