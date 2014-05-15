d09aca104a46af54df4136faadff35900855ea33
Matt Mehlhope <mmehlhope@gmail.com>
2014-05-15 08:32:46 -0700

    New sign ups redirected to organization page

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,2 +1,12 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+
+  protected
+
+  def after_sign_up_path_for(resource)
+    new_organization_path
+  end
+
+  def after_inactive_sign_up_path_for(resource)
+    new_organization_path
+  end
 end
