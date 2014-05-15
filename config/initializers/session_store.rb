f5fa814f49d60cd76abb96f3e2700ca110d09127
Matt Mehlhope <mmehlhope@gmail.com>
2014-05-14 18:11:42 -0700

    Fixing session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-LadderManager::Application.config.session_store :cookie_store, key: '_your_app_session', domain: ".laddermanager.com"
+LadderManager::Application.config.session_store :cookie_store, key: '_ladder_manager'
