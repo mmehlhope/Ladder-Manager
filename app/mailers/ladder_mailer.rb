668f5e8f1c12bcad5e42d65f364ccf28ef6d0697
Matt Mehlhope <mmehlhope@gmail.com>
2014-06-01 23:21:59 -0700

    LadderMailer is now the primary mailer

--- app/mailers/ladder_mailer.rb
+++ app/mailers/ladder_mailer.rb
@@ -1,28 +1,38 @@
-class LadderMailer < ActionMailer::Base
+class LadderMailer < Devise::Mailer
+  helper :application
+  include Devise::Controllers::UrlHelpers
+
   default from: "info@laddermanager.com"
 
-  def welcome_email(user)
-    @user = user
-    @organization = @user.organization
-    @url  = "http://laddermanager.com/organizations/#{@organization.id}"
-    mail(to: @user.email, subject: "Welcome to LadderManager, #{@user.name}")
+  def invitation_email(user, password)
+    @user     = user
+    @password = password
+    mail(to: @user.email, subject: "You are now an admin for #{@user.organization.name}!")
   end
 
-  def new_ladder_email(user, ladder)
-    @ladder = ladder
-    @url  = "http://laddermanager.com/ladders/#{@ladder.id}"
-    mail(from: "new-ladder@laddermanager.com", to: @ladder.user.email, subject: "Your new ladder: #{@ladder.name}")
-  end
+  # def welcome_email(user)
+  #   @user = user
+  #   @organization = @user.organization
+  #   @url  = "http://laddermanager.com/organizations/#{@organization.id}"
+  #   mail(to: @user.email, subject: "Welcome to LadderManager, #{@user.name}")
+  # end
 
-  def password_changed(ladder)
-    @ladder = ladder
-    @url  = "http://laddermanager.com/ladders/#{@ladder.id}"
-    mail(to: @ladder.user.email, subject: "Password changed for #{current_user.name}")
-  end
 
-  def activation_email(ladder)
-    @ladder = ladder
-    @url  = "http://laddermanager.com/ladders/#{@ladder.id}"
-    mail(to: @ladder.user.email, subject: "Password changed for #{current_user.name}")
-  end
+  # def new_ladder_email(user, ladder)
+  #   @ladder = ladder
+  #   @url  = "http://laddermanager.com/ladders/#{@ladder.id}"
+  #   mail(from: "new-ladder@laddermanager.com", to: @ladder.user.email, subject: "Your new ladder: #{@ladder.name}")
+  # end
+
+  # def password_changed(ladder)
+  #   @ladder = ladder
+  #   @url  = "http://laddermanager.com/ladders/#{@ladder.id}"
+  #   mail(to: @ladder.user.email, subject: "Password changed for #{current_user.name}")
+  # end
+
+  # def activation_email(ladder)
+  #   @ladder = ladder
+  #   @url  = "http://laddermanager.com/ladders/#{@ladder.id}"
+  #   mail(to: @ladder.user.email, subject: "Password changed for #{current_user.name}")
+  # end
 end
