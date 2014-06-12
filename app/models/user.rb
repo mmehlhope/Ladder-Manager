c149143c6c92947c5eef6acdb9c41c1c78a396ff
Matt Mehlhope <mmehlhope@gmail.com>
2014-06-11 21:53:08 -0700

    Remove cruft from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,13 +8,6 @@ class User < ActiveRecord::Base
 
   belongs_to :organization
 
-  # validates :name, presence: true, format: {
-  #   with: /\A[a-zA-Z0-9]*[a-zA-Z][a-zA-Z0-9' ]*\z/, message: "can only contain letters, numbers, and spaces."
-  # }
-  # validates :email, presence: true, uniqueness: true, format: {
-  #   with: /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/, message: "must be a valid email address."
-  # }
-
   def can_delete_user?(user_id)
     if id == user_id.to_i
       errors.add(:base, "You cannot delete yourself")
