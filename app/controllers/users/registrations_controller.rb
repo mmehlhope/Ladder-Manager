08aed2348fc67e7e78b89eb48d4537f65795658f
Matt Mehlhope <mmehlhope@gmail.com>
2014-06-18 08:23:30 -0700

    Name is now a permitted user registration field

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,11 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  before_filter :configure_permitted_parameters, :only => [:create]
 
   protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
+  end
 
   def after_sign_up_path_for(resource)
     new_organization_path
