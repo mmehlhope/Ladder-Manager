8564632b31fe311c08773a4b3ab110713ad04915
Matt Mehlhope <mmehlhope@gmail.com>
2014-07-04 23:12:41 -0700

    Fixing typo with validates versus validate

--- app/models/ladder.rb
+++ app/models/ladder.rb
@@ -9,8 +9,8 @@ class Ladder < ActiveRecord::Base
   validates :name, presence: true, format: {
     with: /\A[a-zA-Z0-9]*[a-zA-Z][a-zA-Z0-9  ']*\z/, message: "can only contain letters, numbers, and spaces."
   }
-  validates :within_match_limits
-  validates :within_competitor_limits
+  validate :within_match_limits
+  validate :within_competitor_limits
   validates_associated :organization, message: "has reached the maximum number of allowed ladders. Email contact@laddermanager.com to request a higher limit."
   
   MATCH_LIMIT = 500
